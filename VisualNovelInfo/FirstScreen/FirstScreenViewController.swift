5689f93e11cf907e9a889d8a4af546cc3ede5299
Daniel Alberto Vázquez Rodríguez <danielunam1511@gmail.com>
2025-06-14 14:09:42 -0600

    Crear primer view controller para el proyecto

---
+++ VisualNovelInfo/FirstScreen/FirstScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstScreenViewController.swift
+//  VisualNovelInfo
+//
+//  Created by Daniel Alberto Vazquez Rodriguez on 14/06/25.
+//
+
+import UIKit
+
+class FirstScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
