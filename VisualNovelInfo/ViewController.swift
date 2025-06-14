93ec777e6c2565dd003df02cdb362f88411ab872
Daniel Alberto Vázquez Rodríguez <danielunam1511@gmail.com>
2025-06-14 13:54:00 -0600

    Initial Commit

---
+++ VisualNovelInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisualNovelInfo
+//
+//  Created by Daniel Alberto Vazquez Rodriguez on 14/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
